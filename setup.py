19715380a23dd7060316531655e4a6ba70e5d674
Luis C. Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2023-03-22 19:25:18 +0000

    v0.7.0.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(name='dockerx',
-    version='0.6.0',
+    version='0.7.0',
     description='Launcher of Docker containers.',
     author='Luis C. Garcia-Peraza Herrera',
     author_email='luiscarlos.gph@gmail.com',
