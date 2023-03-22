29b7a43197d23bbc370ce3ce3013256ddbdd0ee6
Luis C. Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2023-03-22 16:55:40 +0000

    Adding Python version check.

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,9 @@
 __version__='0.6.0'
+
+import sys
+
+if sys.version_info < (3, 9):
+    error_msg = '[ERROR] The dockerx package requires at least Python 3.9.'
+    raise RuntimeError(error_msg)
+
 from .dl import *
